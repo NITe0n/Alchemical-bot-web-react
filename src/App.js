0553cba80aca462738cd43108b84bfcd73c788a0
NITe <gt60m@mail.ru>
2024-02-02 18:04:55 +0300

    add header and button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+const tg = window.Telegram.WebApp;
 
 function App() {
+
+  const onClose = () => {
+    tg.close()
+  }
 
   return (
     <div className="App">
       work
+      <button>Закрыть</button>
     </div>
   );
 }
 
 export default App;
+
